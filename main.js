4a11127961d894f0d3997c02d0cd0d07f474c88b
BabyGirlVeeh <vahny15@gmail.com>
2022-08-04 16:47:18 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
+const logoImage = document.querySelector(".logo img");
+
+menuBtn.addEventListener("click", function() {
+    menuBtn.classList.toggle("active");
+    nav.classList.toggle("active");
+}
+)
+
+window.addEventListener("scroll", function() {
+    if(this.window.pageYOffset > 4) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+}
+)
